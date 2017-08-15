becd5e88ed8cb6bceab0c9818fe27bb40a6dd43e
Vladimir Shchur <vladimir.shchur@idt.net>
2017-08-15 22:19:37 +0300

    Added patched kestrel lib

--- build.ps1
+++ build.ps1
@@ -1,6 +1,7 @@
 Set-Location src
 .\build.cmd
 Set-Location ..
+Copy-Item Microsoft.AspNetCore.Server.Kestrel.dll artifacts
 docker build -t hcup/odin .
 # docker login stor.highloadcup.ru
 docker tag hcup/odin stor.highloadcup.ru/travels/safe_pike
