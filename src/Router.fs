adcc3f6ae8ed83786e978f4ece63bf53a0f38c1b
Vladimir Shchur <vladimir.shchur@idt.net>
2017-08-23 12:24:46 +0300

    Router perf improvements

--- src/Router.fs
+++ src/Router.fs
@@ -9,28 +9,8 @@ open Juraff.HttpHandlers
 open Juraff.FormatExpressions
 open Juraff.Common
 
-
 type IdHandler = int -> HttpFunc -> HttpContext -> HttpFuncResult
 type IdHandlers = Dictionary<string, IdHandler>
-
-let (|IsVisit|_|) (path: PathString) =
-   let remaining = ref PathString.Empty
-   if (path.StartsWithSegments(PathString("/visits"), remaining))
-   then Some(remaining)
-   else None
-
-let (|IsUser|_|) (path: PathString) =
-   let remaining = ref PathString.Empty
-   if (path.StartsWithSegments(PathString("/users"), remaining))
-   then Some(remaining)
-   else None
-
-let (|IsLocation|_|) (path: PathString) =
-   let remaining = ref PathString.Empty
-   if (path.StartsWithSegments(PathString("/locations"), remaining))
-   then Some(remaining)
-   else None
-
 
 let tryParseId stringId path (dictIdHandler: IdHandlers) next ctx =
    match Int32.TryParse(stringId) with
@@ -41,20 +21,21 @@ let customRoutef (dictIdHandler: IdHandlers) : HttpHandler =
     fun (next : HttpFunc) (ctx : HttpContext) ->
         let remaining = ref PathString.Empty
         match ctx.Request.Path with
-        | IsVisit path -> tryParseId (path.Value.Value.Substring(1)) "/visits/%i" dictIdHandler next ctx
-        | IsUser path -> 
-            let pathString = path.Value.Value.Substring(1)
-            if (pathString.EndsWith("/visits"))
+        | visitPath when (visitPath.StartsWithSegments(PathString("/visits"), remaining)) ->
+             tryParseId (remaining.Value.Value.Substring(1)) "/visits/%i" dictIdHandler next ctx
+        | userPath when (userPath.StartsWithSegments(PathString("/users"), remaining)) -> 
+            let pathString = remaining.Value.Value.Substring(1)
+            let slashIndex = pathString.IndexOf("/visits")
+            if (slashIndex > -1)
             then
-                let slashIndex = pathString.IndexOf("/") 
                 tryParseId (pathString.Substring(0,slashIndex)) "/users/%i/visits" dictIdHandler next ctx
             else 
                 tryParseId pathString "/users/%i" dictIdHandler next ctx
-        | IsLocation path ->         
-            let pathString = path.Value.Value.Substring(1)
-            if (pathString.EndsWith("/avg"))
+        | locationPath when (locationPath.StartsWithSegments(PathString("/locations"), remaining)) ->          
+            let pathString = remaining.Value.Value.Substring(1)
+            let slashIndex = pathString.IndexOf("/avg")
+            if (slashIndex > -1)
             then
-                let slashIndex = pathString.IndexOf("/") 
                 tryParseId (pathString.Substring(0,slashIndex)) "/locations/%i/avg" dictIdHandler next ctx
             else 
                 tryParseId pathString "/locations/%i" dictIdHandler next ctx
