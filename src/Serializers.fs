fe2a3972447e7c6866ec99a3f7c2619947b76aa5
Vladimir Shchur <vladimir.shchur@idt.net>
2017-09-22 11:35:08 +0300

    Added StringBuilderCache for small strings

--- src/Serializers.fs
+++ src/Serializers.fs
@@ -5,17 +5,17 @@ open HCup.Models
 open HCup
 
 let inline serializeLocation (loc: Location) =
-    let sb = StringBuilderCache.Acquire()
+    let sb = StringBuilderCache.Acquire(SbSize.Small)
                     .Append("{\"id\":").Append(loc.id.ToString(System.Globalization.CultureInfo.InvariantCulture))
                     .Append(",\"distance\":").Append(loc.distance.ToString(System.Globalization.CultureInfo.InvariantCulture))
                     .Append(",\"place\":\"").Append(loc.place)
                     .Append("\",\"city\":\"").Append(loc.city)
                     .Append("\",\"country\":\"").Append(loc.country)
                     .Append("\"}")
-    StringBuilderCache.GetStringAndRelease(sb)
+    StringBuilderCache.GetStringAndRelease sb SbSize.Small
 
 let inline serializeUser (user: User) =
-    let sb = StringBuilderCache.Acquire()
+    let sb = StringBuilderCache.Acquire(SbSize.Small)
                     .Append("{\"id\":").Append(user.id.ToString(System.Globalization.CultureInfo.InvariantCulture))
                     .Append(",\"birth_date\":").Append(user.birth_date.ToString(System.Globalization.CultureInfo.InvariantCulture))
                     .Append(",\"first_name\":\"").Append(user.first_name)
@@ -23,20 +23,20 @@ let inline serializeUser (user: User) =
                     .Append("\",\"gender\":\"").Append(user.gender.ToString())
                     .Append("\",\"email\":\"").Append(user.email)
                     .Append("\"}")
-    StringBuilderCache.GetStringAndRelease(sb)
+    StringBuilderCache.GetStringAndRelease sb SbSize.Small
 
 let inline serializeVisit (visit: Visit) =
-    let sb = StringBuilderCache.Acquire()
+    let sb = StringBuilderCache.Acquire(SbSize.Small)
                     .Append("{\"id\":").Append(visit.id.ToString(System.Globalization.CultureInfo.InvariantCulture))
                     .Append(",\"location\":").Append(visit.location.ToString(System.Globalization.CultureInfo.InvariantCulture))
                     .Append(",\"mark\":").Append(visit.mark.ToString(System.Globalization.CultureInfo.InvariantCulture))
                     .Append(",\"user\":").Append(visit.user.ToString(System.Globalization.CultureInfo.InvariantCulture))
                     .Append(",\"visited_at\":").Append(visit.visited_at.ToString(System.Globalization.CultureInfo.InvariantCulture))
                     .Append("}")
-    StringBuilderCache.GetStringAndRelease(sb)
+    StringBuilderCache.GetStringAndRelease sb SbSize.Small
 
 let inline serializeVisits (visits: seq<UserVisit>) =
-    let sb = StringBuilderCache.Acquire().Append("{\"visits\":[")
+    let sb = StringBuilderCache.Acquire(SbSize.Big).Append("{\"visits\":[")
     let mutable start = true
     for visit in visits do
         (if start
@@ -49,10 +49,10 @@ let inline serializeVisits (visits: seq<UserVisit>) =
           .Append(",\"visited_at\":").Append(visit.visited_at)
           .Append(",\"place\":\"").Append(visit.place)
           .Append("\"}") |> ignore
-    StringBuilderCache.GetStringAndRelease(sb.Append("]}"))
+    StringBuilderCache.GetStringAndRelease (sb.Append("]}")) SbSize.Big
 
 let inline serializeAvg (avg: float) =
-    let sb = StringBuilderCache.Acquire()
+    let sb = StringBuilderCache.Acquire(SbSize.Small)
                 .Append("{\"avg\":").Append(avg.ToString(System.Globalization.CultureInfo.InvariantCulture))
                 .Append("}")
-    StringBuilderCache.GetStringAndRelease(sb)  
+    StringBuilderCache.GetStringAndRelease sb SbSize.Small  
