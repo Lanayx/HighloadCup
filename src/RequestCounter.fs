06b577fe8ce804ea820c9e4d67db0d51d0d372c7
Friedrich von Never <friedrich@fornever.me>
2017-09-25 14:56:59 +0700

    RequestCounter: remove excessive task allocation

--- src/RequestCounter.fs
+++ src/RequestCounter.fs
@@ -15,23 +15,20 @@ type RequestCounterMiddleware (next : RequestDelegate,
                                handler : HttpHandler) =
 
     member __.Invoke (ctx : HttpContext) =
-        task {
-            let! result = next.Invoke ctx
-            Interlocked.Increment(outstandingRequestCount)
-            |> (fun reqCount -> 
-                                if (reqCount = 150150 || reqCount = 190150)
-                                then GC.Collect(1)
-                                if (reqCount &&& 8191 = 0)
-                                then
-                                    Console.WriteLine("Gen0={0} Gen1={1} Gen2={2} Alloc={3} Time={4} ReqCount={5}",
-                                        GC.CollectionCount(0),
-                                        GC.CollectionCount(1),
-                                        GC.CollectionCount(2),
-                                        GC.GetTotalMemory(false),                                        
-                                        DateTime.Now.ToString("HH:mm:ss.ffff"),
-                                        reqCount))
-        } :> Task
-
+        Interlocked.Increment(outstandingRequestCount)
+        |> (fun reqCount -> 
+                            if (reqCount = 150150 || reqCount = 190150)
+                            then GC.Collect(1)
+                            if (reqCount &&& 8191 = 0)
+                            then
+                                Console.WriteLine("Gen0={0} Gen1={1} Gen2={2} Alloc={3} Time={4} ReqCount={5}",
+                                    GC.CollectionCount(0),
+                                    GC.CollectionCount(1),
+                                    GC.CollectionCount(2),
+                                    GC.GetTotalMemory(false),                                        
+                                    DateTime.Now.ToString("HH:mm:ss.ffff"),
+                                    reqCount))
+        next.Invoke ctx
 
 type IApplicationBuilder with
     member this.UseRequestCounter (handler : HttpHandler) =
