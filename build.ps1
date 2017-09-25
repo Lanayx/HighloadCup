2efff97f7073607d834dd796f21cb64f247aa2cd
Friedrich von Never <friedrich@fornever.me>
2017-09-25 22:46:38 +0700

    Tune the environment

--- build.ps1
+++ build.ps1
@@ -1,9 +1,12 @@
+$container = if ($env:COMPUTERNAME -eq 'CTHULHU-3') { 'hcup/fornever' } else { 'hcup/odin' }
+$tag = if ($env:COMPUTERNAME -eq 'CTHULHU-3') { 'stor.highloadcup.ru/travels/nice_toucan' } else { 'stor.highloadcup.ru/travels/safe_pike' }
+
 Set-Location src
 .\build.cmd
 Set-Location ..
 get-childitem -path ".\kestrel" -recurse | copy-item -destination ".\artifacts"
-docker build -t hcup/odin .
-# docker run -it --rm -p 80:80 hcup/odin
+docker build -t $container .
+# docker run -it --rm -p 80:80 $container
 # docker login stor.highloadcup.ru
-docker tag hcup/odin stor.highloadcup.ru/travels/safe_pike
-docker push stor.highloadcup.ru/travels/safe_pike
+docker tag $container $tag
+docker push $tag
