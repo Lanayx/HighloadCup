ac0be9c7a60194a108c65d78c4652fbca91d4f32
Vladimir Shchur <vladimir.shchur@idt.net>
2017-09-28 15:59:34 +0300

    Improved GC timer logging, added warming calls

--- src/GCTimer.fs
+++ src/GCTimer.fs
@@ -1,10 +1,12 @@
 module GCTimer
 
 open System
+open System.Net.Http
+
 let outstandingRequestCount = ref 0
 let mutable lastRequestCount = 0
 let mutable GCRun = false
-
+let client = new HttpClient()
 
 let syncTimer = new System.Timers.Timer(500.0)
 syncTimer.Elapsed.Add(fun arg ->
@@ -12,9 +14,10 @@ syncTimer.Elapsed.Add(fun arg ->
     then
         if not GCRun
         then
-            Console.WriteLine("Running GC {0} {1}", lastRequestCount, outstandingRequestCount.Value)
+            Console.WriteLine("Running GC {0} {1} {2}", lastRequestCount, outstandingRequestCount.Value, DateTime.Now.ToString("HH:mm:ss.ffff"))
             GCRun <- true
             GC.Collect(1)
+        client.GetAsync("http://127.0.0.1/visits/8").Result |> ignore            
     else
         GCRun <- false
     lastRequestCount <- outstandingRequestCount.Value
